84fe5a457da6867c8b7176fba494ca20ff7edb0d
Syed <srahmaan7062@gmail.com>
2025-04-20 19:58:18 -0400

    added menu and option 1

--- src/main/java/com/ps/LibraryApp.java
+++ src/main/java/com/ps/LibraryApp.java
@@ -14,6 +14,29 @@ public class LibraryApp {
                 "Flame", "Haven", "Spark", "Thorn", "Quest"
         };
 
+        for (int i = 0; i < library.length; i++) {
+            library[i] = new Book(i+1, "100"+i, books[i]);
+//            System.out.println(library[i].getTitle());
+        }
 
+        boolean running = true;
+
+        while(running) {
+            System.out.println("\n- Main Menu -");
+            System.out.println("[1] Show Available Books - ");
+            System.out.println("[2] Show Checked Out Books - ");
+            System.out.println("[3] Exit Menu - ");
+            System.out.println("Choose an option: ");
+            String choice = scanner.nextLine();
+
+            if (choice.equals("1")) {
+                System.out.println("Available Books: ");
+                for (Book book : library) {
+                    if (!book.isCheckedOut()) {
+                        System.out.println(" ID: " + book.getId() +" ISBN: " + book.getIsbn() +" Title: " + book.getTitle());
+                    }
+                }
+            }
+        }
     }
 }
